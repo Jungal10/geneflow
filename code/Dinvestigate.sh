df8c6f36aa4790be5341128811c74ecfe590aa5a
Jungal10 <j.goncalvesdias@uni-koeln.de>
2022-02-25 13:20:03 +0100

    updated dsuite script

--- code/Dinvestigate.sh
+++ code/Dinvestigate.sh
@@ -5,12 +5,13 @@
 #SBATCH --mem=46gb
 #SBATCH --account=UniKoeln
 #SBATCH --mail-user=jgoncal1@uni-koeln.de
-#SBATCH --error /scratch/jgoncal1/logs/errors/Dsuite_Dinvestigate_w100_allchr_crops_trios_%j.err
+#SBATCH --error /scratch/jgoncal1/logs/errors/Dsuite_Dinvestigate_w100_full_genome_crops_trios_%j.err
 #SBATCH -D /projects/ag-stetter/jdias/projects/geneflow/code/
-#SBATCH -o /scratch/jgoncal1/logs/Dsuite_Dinvestigate_w100_allchr_crops_trios_%j.log
-#SBATCH --job-name="Dsuite_Dinvestigate_w100_allchr_crops_trios_" # a nice readable name to give your job so you know what it is when you see it in the queue, instead of just numbers
+#SBATCH -o /scratch/jgoncal1/logs/Dsuite_Dinvestigate_w100_full_genome_crops_trios_%j.log
+#SBATCH --job-name="Dsuite_Dinvestigate_w100_full_genome_crops_trios_" # a nice readable name to give your job so you know what it is when you see it in the queue, instead of just numbers
 
 source /home/jgoncal1/.bashrc
+module load miniconda/py38_4.9.2
 conda activate base_jgd
 module load gnu/7.4.0
 
---
+++ code/Dtrios_Dsuite.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -l
+#SBATCH --partition=smp-rh7 
+#SBATCH --time=4:00:00
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=46gb
+#SBATCH --account=UniKoeln
+#SBATCH --mail-user=jgoncal1@uni-koeln.de
+#SBATCH --error /scratch/jgoncal1/logs/errors/Dsuite_Dtrios_full_genome_%j.err
+#SBATCH -D /projects/ag-stetter/jdias/projects/GeneFlow/code/
+#SBATCH -o /scratch/jgoncal1/logs/Dsuite_Dtrios_full_genome_%j.log
+#SBATCH --job-name="Dsuite_Dtrios_full_genome_" # a nice readable name to give your job so you know what it is when you see it in the queue, instead of just numbers
+
+source /home/jgoncal1/.bashrc
+module load miniconda/py38_4.9.2
+conda activate base_jgd
+module load gnu/7.4.0
+
+Dsuite Dtrios \
+../data/processed/vcf_files/ready_dovcf_geneflowsamples_all_chr.vcf.gz \
+file_lists/list_amaranth_geneflow_samples_populations.txt  \
+-n full_genome \
+-o ../data/processed/Dtrios -c
